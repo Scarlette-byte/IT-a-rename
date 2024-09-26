fcfaf45d3329a7932df1625b50d640457f9d0476
Scarlette_byte <mancawanscarlette36@gmail.com>
2024-09-26 15:04:27 +0800

    added string palindrome

--- app.js
+++ app.js
@@ -50,3 +50,14 @@ function checkPrime(num) {
 }
 
 console.log(checkPrime(13));
+// Determine if a string is a palindrome
+function checkPalindrome(str) {
+    let reversedStr = str.split('').reverse().join('');
+    if (str === reversedStr) {
+        return "Palindrome";
+    } else {
+        return "Not a Palindrome";
+    }
+}
+
+console.log(checkPalindrome(""));
